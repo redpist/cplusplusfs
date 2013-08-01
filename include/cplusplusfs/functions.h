a5cd7003f6d4718c22c04cbca7e3860bed031df6
Pierre-Yves Le Borgne <pierre-yves.le-borgne@epitech.eu>
2013-08-01 18:00:22 +0200

    added cplusplusfs/functions.h containing global utility functions

---
+++ include/cplusplusfs/functions.h
@@ -0,0 +1,13 @@
+#ifndef CPLUSPLUSFS_CPLUSPLUSFS_FUNCTIONS_H_
+#define CPLUSPLUSFS_CPLUSPLUSFS_FUNCTIONS_H_
+
+namespace cplusplusfs {
+
+class File;
+
+File CurrentDirectory();
+File CurrentPath();
+
+}
+
+#endif  // CPLUSPLUSFS_CPLUSPLUSFS_FUNCTIONS_H_
